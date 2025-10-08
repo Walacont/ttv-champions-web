a139cb2b32859f6b254cb52b36637daf9b7a0efb
Fishcakemonster <tommy.wang@haw-hamburg.de>
2025-10-08 13:36:07 +0200

    öffentliche Version

---
+++ public/js/firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD0fZWqLVsw1ah7v5snSdqsFX1_Q1BDSOg",
+  authDomain: "ttv-champions-web.firebaseapp.com",
+  projectId: "ttv-champions-web",
+  storageBucket: "ttv-champions-web.firebasestorage.app",
+  messagingSenderId: "296518698973",
+  appId: "1:296518698973:web:247dfce5935eefad841834",
+  measurementId: "G-M14FDM8XYC"
+};
+
